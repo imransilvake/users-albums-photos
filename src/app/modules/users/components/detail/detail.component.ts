86fc4ae5ecd5ae003436fda4e756665e4956bc00
Imran Khan <imransilvake@gmail.com>
2023-04-04 23:55:19 +0200

    fix: Cache albums api call

--- src/app/modules/users/components/detail/detail.component.ts
+++ src/app/modules/users/components/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { firstValueFrom, lastValueFrom, Observable } from 'rxjs';
 import { AppStateInterface } from 'src/app/types/appState.interface';
 import {
 	isLoaderSelector,
@@ -29,6 +29,8 @@ export class DetailComponent implements OnInit {
 	userAlbumsPhotos$: Observable<UserAlbumsPhotosInterface>;
 
 	albumIdx = -1;
+	albumsPhotos: UserAlbumsPhotosInterface | null = null;
+
 	constructor(private store: Store<AppStateInterface>) {
 		this.isLoader$ = this.store.pipe(select(isLoaderSelector));
 		this.userAlbums$ = this.store.pipe(select(userAlbumsSelector));
@@ -38,15 +40,18 @@ export class DetailComponent implements OnInit {
 		this.userAlbumsPhotos$ = this.store.pipe(
 			select(userAlbumsPhotosSelector)
 		);
+
+		this.userAlbumsPhotos$.subscribe((res) => (this.albumsPhotos = res));
 	}
 
-	ngOnInit(): void {
+	async ngOnInit(): Promise<void> {
+		// user id, user albums
+		const userId = +(location.pathname?.split('/')?.pop() || 0);
+		const userAlbums = await firstValueFrom(this.userAlbums$);
+
 		// dispatch: fetch user albums
-		this.userAlbums$.subscribe((userAlbums) => {
-			if (userAlbums?.length) return;
-			const userId = +(location.pathname?.split('/')?.pop() || 0);
-			this.store.dispatch(UsersActions.getUserAlbums({ userId }));
-		});
+		if (userAlbums?.length && userAlbums?.[0]?.userId === userId) return;
+		this.store.dispatch(UsersActions.getUserAlbums({ userId }));
 	}
 
 	/**
